dea286175e132d7458afdaa5eff623232aa0bd5d
cjl <chen@qq.com>
2025-03-28 09:13:06 +0800

    20250328:PartC

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Raft
 Raft of Go
+
+
+## 领导者选举
+1、状态转换（三个转换函数）
+    1）becomeFollowerLocked
+    2）becomeCandidateLocked
+    3）becomeLeaderLocked
+2、选举逻辑
+    1）选举的循环
+    2）单轮选举
+    3）单次RPC
+    4）请求回应
+3、心跳逻辑
+    1）心跳的循环
+    2）单轮心跳
+    3）单词RPC
+    4）请求回应
+
+## 日志复制
--- src/raft/raft_replication.go
+++ src/raft/raft_replication.go
@@ -26,6 +26,10 @@ type AppendEntriesArgs struct{
 type AppendEntriesReply struct{
 	Term		int
 	Success		bool
+
+	// 为了更快的日志匹配，在回复的时候多加了以下两个字段
+	ConfilictIndex int		// 冲突的索引号
+	ConfilictTerm  int		// 冲突的任期号
 }
 
 // reply为传出参数
@@ -45,16 +49,23 @@ func (rf *Raft)AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply)
 		rf.becomeFollowerLocked(args.Term)
 	}
 
+	// 重置选举超时时间，表示我不再争取作为leader，因为你已经是我的leader
+	defer rf.resetElectionTimerLocked()
+
 	// 本地的日志太久没有与leader同步了
 	// 检查args.PrevLogIndex是否超出了跟随者的日志长度。如果超出，说明跟随者的日志不够长，拒绝日志追加并记录日志。
+	// 需要记录具体的任期和日志中信息进行回复
 	if args.PrevLogIndex >= len(rf.log){
+		reply.ConfilictIndex = len(rf.log)
+		reply.ConfilictTerm = InvalidIndex
 		LOG(rf.me,rf.currentTerm,DLog2,"<- S%d,Reject log,Follower too short,len : %d <= Pre:%d",args.LeaderId,len(rf.log),args.PrevLogIndex)
 		return
 	}
 	// 代码执行到这里说明此时leader和follower的日志号已经一致
 	// 检查任期号是否一致，只有日志号和任期号一致才可以返回成功
-	
 	if rf.log[args.PrevLogIndex].Term != args.PrevLogTerm{
+		reply.ConfilictTerm = rf.log[args.PrevLogIndex].Term
+		reply.ConfilictIndex = rf.firstLogFor(reply.ConfilictTerm) // 找到这个任期的第一个日志
 		LOG(rf.me,rf.currentTerm,DLog2,"<- S%d,Reject log,Pre Log not match,[%d]: T%d != T%d",args.LeaderId,args.PrevLogTerm,rf.log[args.PrevLogIndex].Term)
 		return
 	}
@@ -75,10 +86,6 @@ func (rf *Raft)AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply)
 		}
 		rf.applyCond.Signal() // 发送信号准备提交
 	}
-
-	// 重置选举超时时间，表示我不再争取作为leader，因为你已经是我的leader
-	rf.resetElectionTimerLocked()
-
 }
 
 func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
@@ -120,17 +127,39 @@ func (rf *Raft)startReplication(term int) bool{
 		}
 
 		// 如果回复失败，说明日志号不对，需要再向前一个任期进行检查
+		// if !reply.Success{
+		// 	// 记录一下当前最后一个日志号
+		// 	idx := rf.nextIndex[peer] - 1
+		// 	// 记录一下当前的最后一个任期号
+		// 	term := rf.log[idx].Term
+		// 	for idx > 0 && rf.log[idx].Term == term{
+		// 		idx-- //日志号减1
+		// 	}
+		// 	// 将下一个日志号改为idx + 1，说明减少了一个任期准备下次再次进行对比
+		// 	rf.nextIndex[peer] = idx + 1
+		// 	LOG(rf.me,rf.currentTerm,DLog,"Log not matched in %d,Update next = %d",args.PrevLogIndex,rf.nextIndex[peer])
+		// 	return
+		// }
+
+		// 如果回复失败，说明日志号不对，需要进行检查
 		if !reply.Success{
-			// 记录一下当前最后一个日志号
-			idx := rf.nextIndex[peer] - 1
-			// 记录一下当前的最后一个任期号
-			term := rf.log[idx].Term
-			for idx > 0 && rf.log[idx].Term == term{
-				idx-- //日志号减1
+			prevIndex := rf.nextIndex[peer]
+			// 如果任期为空
+			if reply.ConfilictTerm == InvalidTerm{
+				// 说明follower的日志太短，直接将nextIndex赋值为ConfilictIndex退到Follower日志末尾
+				rf.nextIndex[peer] = reply.ConfilictIndex
+			}else{ // 否则，以 Leader 日志为准，跳过 ConfilictTerm 的所有日志
+				firstTermIndex := rf.firstLogFor(reply.ConfilictTerm)
+				if firstTermIndex != InvalidTerm{
+					rf.nextIndex[peer] = firstTermIndex + 1
+				}else{ //如果发现Leader日志中不存在ConfilictTerm的任何日志，则以Follower为准跳过ConflictTerm，即使用ConfilictIndex
+					rf.nextIndex[peer] = reply.ConfilictIndex
+				}
 			}
-			// 将下一个日志号改为idx + 1，说明减少了一个任期准备下次再次进行对比
-			rf.nextIndex[peer] = idx + 1
-			LOG(rf.me,rf.currentTerm,DLog,"Log not matched in %d,Update next = %d",args.PrevLogIndex,rf.nextIndex[peer])
+			// 设置为prevNext和rf.nextIndex[peer]最小的，防止较快的RPC会覆盖掉原来的小的值
+			if rf.nextIndex[peer] > prevIndex {
+                rf.nextIndex[peer] = prevIndex
+       		 }
 			return
 		}
 
@@ -140,12 +169,12 @@ func (rf *Raft)startReplication(term int) bool{
 		rf.nextIndex[peer] = rf.matchIndex[peer] + 1
 		// TODO：更新commitIndex
 		majorityMactched := rf.getMajorityIndexLocked()
-		if majorityMactched > rf.commitIndex{
+		// 当日志复制过半且提交的日志对应的任期为当前的任期时候才可以提交
+		if majorityMactched > rf.commitIndex && rf.log[majorityMactched].Term == rf.currentTerm{
 			LOG(rf.me,rf.currentTerm,DApply,"leader update commit index %d -> %d",rf.commitIndex,majorityMactched)
 			rf.commitIndex = majorityMactched	// 更新commitIndex的值，用于applyTicker遍历的时候使用
 			rf.applyCond.Signal()	// 唤醒applyTicker中的睡眠，将已提交但未应用的日志发送到applyCh，供状态机执行
 		}
-		
 	}
 	rf.mu.Lock()
 	defer rf.mu.Unlock()
@@ -171,7 +200,6 @@ func (rf *Raft)startReplication(term int) bool{
 			Entries:rf.log[prevIndex+1:], // 从 rf.log 切片中截取从索引 prevIndex+1 开始到末尾的所有日志条目。左闭右开
 			LeaderCommit:rf.commitIndex,
 		}
-
 		go replicateToPeer(peer,args)
 	}
 	return true
@@ -184,7 +212,6 @@ func (rf* Raft)replicationTicker(term int){
 		if !ok{
 			break
 		}
-
 		time.Sleep(replicateInterval)
 	}
 }
